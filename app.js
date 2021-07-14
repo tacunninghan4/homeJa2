f2a479123132f3063ea83411522032ae827c7ac2
Todd <andrewcunningham770@gmail.com>
2021-07-14 07:08:16 -0400

    hey

---
+++ app.js
@@ -0,0 +1,37 @@
+function getAverage(a , b ){
+     var average =(a + b ) / 2
+    console.log(average);
+
+
+
+
+
+}
+
+getAverage(1 , 2);
+
+function getSum(a,b){
+    var Sum =(a+b) 
+counsole.log( sum );
+}
+
+
+getSum(1 , 2);
+
+function getDifference(a , b){
+  var difference =(a - b)
+  console.log( difference);
+}
+
+getDifference(9 - 5);
+
+function getMulti(a , b){
+var Multiple=(a * b)
+console.log(Multiple);
+}
+ 
+getMulti(5 * 5);
+
+
+
+
